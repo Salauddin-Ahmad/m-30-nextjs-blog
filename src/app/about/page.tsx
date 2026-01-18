4ef866d51bb11c6fd344f7ee8ee7a37aa7cb3d2e
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2026-01-18 21:54:04 +0600

    Difference between static and dynamic builds

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default async function AboutLoading() {
   await new Promise((resolve) => setTimeout(resolve, 4000));
   return <div>this is about page</div>;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
-import { ThemeProvider } from "@/providers/ThemeProvider.tsx";
+import { ThemeProvider } from "@/providers/theme-provider";
 // import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
