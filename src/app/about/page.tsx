4bc5d108afdbd84c5dbb308a6b22d423688780d7
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2026-01-18 19:55:41 +0600

    Added loading.tsx and error.tsx files

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
-export default function AboutPage() {
-    return (
-        <div>
-            about page
-        </div>
-    );
+export default async function AboutLoading() {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  return <div>this is about page</div>;
 }
