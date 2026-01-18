1c223ecad564d95a40fdbb15643ee24efaf68d4f
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2026-01-18 17:28:08 +0600

    Global UI – The Root Layout

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Contactlayout({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            <h1>this is contact layout</h1>
+         {children}
+        </div>
+    );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
     return (
         <div>
-            Enter
+            contact page
         </div>
     );
 }
