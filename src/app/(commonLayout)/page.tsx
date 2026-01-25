e1ce5f60969a0034af981c3e0a07d20262c938a8
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2026-01-25 22:09:14 +0600

    Handling url params and fetch options

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,17 @@
 import BlogCard from "@/components/modules/homepage/BlogCard";
-import { Button } from "@/components/ui/button";
 import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types/blog.type";
-import { use } from "react";
 
 export default async function Home() {
-  const { data } = await blogService.getBlogPost();
-
-  console.log(data);
+  const { data } = await blogService.getBlogPost(
+    {
+      // isFeatured: , /boolean
+      search: "", //search by title
+    },
+    {
+      cache: "no-store",
+    },
+  );
   return (
     <div className="grid grid-cols-3 gap-4 max-w-7xl mx-auto px-4">
       {data?.data?.map((post: BlogPost) => (
